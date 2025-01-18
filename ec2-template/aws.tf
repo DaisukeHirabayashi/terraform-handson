672f600ae737fe3d34df6b65966ca830a16703f1
DaisukeHirabayashi <daisuke@pluslab.org>
2025-01-18 15:46:29 +0900

    最低限のosに変える

--- ec2-template/aws.tf
+++ ec2-template/aws.tf
@@ -13,11 +13,40 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
     Name = "ExampleAppServerInstance"
   }
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ssm_instance_profile"
+  role = aws_iam_role.ssm_role.name
 }
