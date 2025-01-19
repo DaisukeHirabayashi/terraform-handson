73f20513a792cced17900d5b5e110a3e26f8a06c
DaisukeHirabayashi <daisuke@pluslab.org>
2025-01-19 21:01:04 +0900

    api gatewayでレスポンスができるように修正

--- ec2-template/api_gateway.tf
+++ ec2-template/api_gateway.tf
@@ -57,3 +57,54 @@ data "aws_iam_policy_document" "private_api" {
     }
   }
 }
+
+resource "aws_api_gateway_resource" "cat" {
+  rest_api_id = aws_api_gateway_rest_api.private.id
+  parent_id   = aws_api_gateway_rest_api.private.root_resource_id
+  path_part   = "cat"
+}
+
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.private.id
+  resource_id   = aws_api_gateway_resource.cat.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cat_integration" {
+  rest_api_id = aws_api_gateway_rest_api.private.id
+  resource_id = aws_api_gateway_resource.cat.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = <<EOF
+{
+   "statusCode" : 200
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.private.id
+  resource_id = aws_api_gateway_resource.cat.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.private.id
+  resource_id = aws_api_gateway_resource.cat.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = <<EOF
+{
+   "id" : 1,
+   "name": "tama"
+}
+EOF  
+  }
+}
+
