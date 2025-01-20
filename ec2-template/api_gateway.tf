186f295788f1735d2a59c75eb0103a4cabceca50
DaisukeHirabayashi <daisuke@pluslab.org>
2025-01-20 10:12:49 +0900

    deployの追加

--- ec2-template/api_gateway.tf
+++ ec2-template/api_gateway.tf
@@ -114,3 +114,31 @@ EOF
   }
 }
 
+resource "aws_api_gateway_deployment" "develop" {
+  rest_api_id = aws_api_gateway_rest_api.private.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.cat.id,
+      aws_api_gateway_method.get_method.id,
+      aws_api_gateway_integration.cat_integration,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "staging" {
+  deployment_id = aws_api_gateway_deployment.develop.id
+  rest_api_id   = aws_api_gateway_rest_api.private.id
+  stage_name    = "staging"
+}
